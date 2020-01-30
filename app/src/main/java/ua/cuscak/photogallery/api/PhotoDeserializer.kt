2479191b0a987f24663fcc0eb115b56759f6c725
Andrej Cuscak <cuscak@gmail.com>
2020-01-30 10:49:37 +0100

    custom deserializer fixed

--- app/src/main/java/ua/cuscak/photogallery/api/PhotoDeserializer.kt
+++ app/src/main/java/ua/cuscak/photogallery/api/PhotoDeserializer.kt
@@ -1,8 +1,10 @@
 package ua.cuscak.photogallery.api
 
+import com.google.gson.Gson
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
+import com.google.gson.reflect.TypeToken
 import ua.cuscak.photogallery.GalleryItem
 import java.lang.reflect.Type
 
@@ -12,13 +14,12 @@ class PhotoDeserializer : JsonDeserializer<PhotoResponse> {
 
     override fun deserialize(json: JsonElement, typeOfT: Type?, context: JsonDeserializationContext?): PhotoResponse {
         // Pull photos object out of JsonElement
-        val photosObj = json.asJsonObject.getAsJsonObject("photos")
+        val photosObj = json.asJsonObject.getAsJsonObject("photos").getAsJsonArray("photo")
         // and convert to PhotoResponse object
-        val x = PhotoResponse()
-        x.galleryItems = photosObj.getAsJsonArray("photo") as List<GalleryItem>
 
-        return x
-
+        return PhotoResponse().apply {
+            galleryItems = Gson().fromJson(photosObj, object : TypeToken<List<GalleryItem>>() {}.type)
+        }
     }
 }
 
